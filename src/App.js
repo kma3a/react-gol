425be2cd823f1e19d7c9ff2d7512a9658cb4e975
Kelsey Ando <kma3a@virginia.edu>
2018-08-25 19:23:40 -0400

    adding the board to the app and making sure you can see something

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Board from './board.js';
 import './App.css';
 
 class App extends Component {
   render() {
+    const currentBoard = new Board();
+    currentBoard.createBoard();
+    var board = currentBoard.showBoard();
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {board}
       </div>
     );
   }
