9788e4a685a685496abf08351ea5783b4686e6ad
Kelsey Ando <kma3a@virginia.edu>
2018-08-25 19:23:17 -0400

    adding the new board and at least one test

--- src/board.js
+++ src/board.js
@@ -2,9 +2,49 @@ import React from 'react';
 import Cell from './cell.js';
 
 
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
+class Board {
+  constructor() {
+    this.boardSize = 9;
+    this.sqroot = Math.sqrt(this.boardSize);
+    this.board = [];
   }
+  createBoard() {
+    var randomInt = (max) => { return Math.floor(Math.random() * Math.floor(max));}
+
+    for(var i=0; i < this.boardSize; i++) {
+      var trueOrFalse = !!randomInt(2);
+      this.board[i] = new Cell(trueOrFalse);
+    }
+  }
+
+  getCell(index) {
+    return this.board[index].renderedDoc();
+  }
+
+
+  showBoard() {
+    return (
+        <div class="board">
+          <div class="row">
+            {this.getCell(0)}
+            {this.getCell(1)}
+            {this.getCell(2)}
+          </div>
+          <div class="row">
+            {this.getCell(3)}
+            {this.getCell(4)}
+            {this.getCell(5)}
+          </div>
+          <div class="row">
+            {this.getCell(6)}
+            {this.getCell(7)}
+            {this.getCell(8)}
+          </div>
+        </div>
+      );
+
+  }
+
 }
 
+export default Board;
--- src/board.spec.js
+++ src/board.spec.js
@@ -1,3 +1,8 @@
 import Board from './board';
+import Cell from './cell.js';
 
-it('should create a board', ()=>{});
+it('should create a board', ()=>{
+  const currentBoard = new Board();
+  currentBoard.createBoard();
+  expect(currentBoard.showBoard().props.children.length).toEqual(3);
+});
