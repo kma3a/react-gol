265d563d038c68830d9cbc0d3ab8c8a06a7cdeef
Kelsey Ando <kma3a@virginia.edu>
2018-08-26 12:05:11 -0400

    adding the code to make it generate the next generation after the push or a button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import Board from './board.js';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    var madeBoard = new Board();
+    madeBoard.createBoard();
+    console.log("I am the board", madeBoard);
+
+    this.state = {
+      board: madeBoard,
+      boardImage: madeBoard.showBoard()
+    };
+  }
+
+  evolve() {
+    console.log("state", this.state);
+    const currentBoard = this.state.board;
+    currentBoard.updateBoard();
+    this.setState({
+      boardImage: currentBoard.showBoard()
+    });
+  }
+
   render() {
-    const currentBoard = new Board();
-    currentBoard.createBoard();
-    var board = currentBoard.showBoard();
     return (
       <div className="App">
-        {board}
+        {this.state.boardImage}
+        <button onClick={()=>{this.evolve()}}> Next Gen </button>
       </div>
     );
   }
